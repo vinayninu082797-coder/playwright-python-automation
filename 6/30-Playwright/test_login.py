1f6430bfc67ffe395f1dcd62b999e780df6dd8d1
Vinay A <vinayninu082797@gmail.com>
2026-07-10 16:48:23 +0530

    Update application URL in login test

--- 6/30-Playwright/test_login.py
+++ 6/30-Playwright/test_login.py
@@ -1,6 +1,11 @@
 from playwright.sync_api import Page, expect
 
 
-def test_google_page(page: Page):
-    page.goto("https://www.google.com/")
-    expect(page).to_have_url("https://www.google.com/")
+def test_login(page: Page):
+    page.goto("https://u-njhtcp.bemcorp.net/Accounts/Account/Account",
+        timeout=60000
+    )
+
+    expect(page).to_have_url(
+        "https://u-njhtcp.bemcorp.net/Accounts/Account/Account"
+    )
